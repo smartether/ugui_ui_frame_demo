b04f1ede0560a9e6a579e8c621a79092eff8f9ec
smartether <smartether@gmail.com>
2018-03-06 16:27:12 +0800

    1,disable ExampleMode

--- Assets/LuaFramework/Scripts/ConstDefine/AppConst.cs
+++ Assets/LuaFramework/Scripts/ConstDefine/AppConst.cs
@@ -10,7 +10,7 @@ namespace LuaFramework {
         /// 如果想删掉框架自带的例子，那这个例子模式必须要
         /// 关闭，否则会出现一些错误。
         /// </summary>
-        public const bool ExampleMode = true;                       //例子模式 
+        public const bool ExampleMode = false;                       //例子模式 
 
         /// <summary>
         /// 如果开启更新模式，前提必须启动框架自带服务器端。
